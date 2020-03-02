c910b6c545eef598803064a79f097ad17d6b9af7
Vlad <vskrashchuk@gmail.com>
2020-03-03 01:09:26 +0300

    Adds basic redux sample

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { combineReducers, createStore } from "redux";
+
+const ADD_TODO = "ADD_TODO";
+
+function visibilityFilter(state = "SHOW_ALL", action) {
+  if (action.type === "SET_VISIBILITY_FILTER") {
+    return action.filer;
+  } else {
+    return state;
+  }
+}
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([
+        {
+          text: action.text,
+          completed: false
+        }
+      ]);
+    case "TOGGLE_TODO":
+      return state.map((todo, index) =>
+        action.index === index ? { ...todo, completed: !todo.completed } : todo
+      );
+    default:
+      return state;
+  }
+}
+
+function addTodo(text) {
+  return {
+    type: ADD_TODO,
+    text
+  };
+}
+
+const reducer = combineReducers({ visibilityFilter, todos });
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(addTodo("Learn Sagas"));
